6f3ab60fbfa5d5f7409ecbcb8178e238f25e4494
Carrosen <carla1.rosen@gmail.com>
2019-04-02 09:57:46 +0200

    refactoring code and seperated methods with private

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,37 +1,29 @@
 class Atm
     attr_accessor :funds
+    require 'date'
 
     def initialize
         @funds = 1000
     end
 
-<<<<<<< HEAD
-    def withdraw(amount)
-      @funds -= amount
-    end
-
-    def withdraw(amount, account)
-      case
-      when amount > account.balance
-        return
-       
-      else
-        @funds -= amount
-        account.balance = account.balance - amount
-        { status: true, message: 'success', date: Date.today, amount: amount }
-  end
-=======
     def withdraw(amount, account)
         case
-        when amount > account.balance
-          return
-         
-        else
-          require 'date'
+        when insufficient_funds_in_account?(amount, account)
+          { status: false, message: 'insufficient funds', date: Date.today }
+         else
+          perform_transaction(amount, account)
+         end
+    end
+
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+      amount > account.balance
+    end 
+
+    def perform_transaction(amount, account)
           @funds -= amount
           account.balance = account.balance - amount
           { status: true, message: 'success', date: Date.today, amount: amount }
-        end
     end
->>>>>>> 7835c64e591b90da9ed2d2246bd36a16bdfab416
 end
