5673fd6851d9c09b10918c9361f48b3371aedba5
Carrosen <carla1.rosen@gmail.com>
2019-04-03 17:02:29 +0200

    added methods for owner

--- lib/account.rb
+++ lib/account.rb
@@ -5,26 +5,32 @@ class Account
     
     
 
-    attr_accessor :pin_code, :exp_date, :account_status
+    attr_accessor :pin_code, :exp_date, :account_status, :owner
 
-    def initialize
+    def initialize(attrs = {})
         @pin_code = rand(1000..9999)
         @exp_date = set_expire_date
         @account_status = :active
-
+        set_owner(attrs[:owner])
     end
 
     def set_expire_date
         Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
-    def self.deactivate(account)
-        account.account_status = :deactivated
-    end
-
     def deactivate
         @account_status = :deactivated
     end
+
+    private
+        def set_owner(obj)
+            obj == nil ? missing_owner : @owner = obj
+        end
+
+        def missing_owner
+            raise "An Account owner is required"
+        end
+
 
 
 end
